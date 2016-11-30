901bc1e837ae3f2be4eac325b6f527a8842e3adc
Josh Hoover <jhoove4@gmail.com>
2016-11-30 13:44:42 -0800

    a few new things

---
+++ liri.js
@@ -0,0 +1,13 @@
+var Twitter = require('./keys.js');
+var userTwitter = new Twitter {
+  consumer_key: 'tPVbFQVgOM1IqkfB1lom8tNoJ',
+  consumer_secret: 'OGMyyyDst1GQoYh15Q5X4INoI1WXdOdmgkPg3X9VH',
+  access_token_key: '784195722119741440-1DHgXPBZbHVyoYwj0Owrlg0PSGDVYIA',
+  access_token_secret: '2HzWCDCSD6nmEeoSaHVoec6DsR1jprHpobTXoFWECU3i6',
+};
+
+var Spotify = require('./key.js');
+var userSpotify new Spotify {
+	clientID: '9c2b75970a36412b8e56399c698ca7c3',
+	clientSecret: 'f6bfed98665e417faafedec798b69131',
+};
